c7362981ada52b0b159e69604c2df14988478ac3
primeministerp <primeministerpete@gmail.com>
2018-07-10 17:16:42 +0000

    updating

--- manifests/files.pp
+++ manifests/files.pp
@@ -78,7 +78,7 @@ class pxe2_ipxe_menus::files(
     mode   => '0777',
     content => '#! /usr/bin/env bash
 CA_PASSWD=$1
-openssl enc -pass pass:${CA_PASSWD} -d -aes256 -in secrets.tar.enc | tar xz -C .',
+openssl enc -pass pass:${CA_PASSWD} -d -aes256 -in secrets.tar.enc | tar xv -C .',
   }
 ->file{[
     "${pxe2_path}/docs",
